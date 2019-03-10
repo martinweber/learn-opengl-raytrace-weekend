af8f76b733a3159990eaee7fe7555c13acbddfd1
Martin Weber <martin.weber@uppercut.at>
2019-03-10 14:00:18 +0100

    implemented sphere list for scene description

--- src/shader/compute.glsl
+++ src/shader/compute.glsl
@@ -26,12 +26,19 @@ struct HitRecord
 	vec3 normal;
 };
 
+const int sphereCount = 2;
+
+Sphere allSpheres[sphereCount] = {
+	Sphere(vec3(0, 0, -1), 0.5),
+	Sphere(vec3(0, -100.5, -1), 100)
+};
+
 vec3 point_at_parameter(in Ray ray, in float t)
 {
 	return ray.origin + t * ray.direction;
 }
 
-bool hit_sphere(in Sphere sphere, in Ray ray, in float t_min, in float t_max, out HitRecord hit)
+bool sphere_hit(in Sphere sphere, in Ray ray, in float t_min, in float t_max, out HitRecord hit)
 {
 	const vec3 oc = ray.origin - sphere.center;
 	const float a = dot(ray.direction, ray.direction);
@@ -63,14 +70,32 @@ bool hit_sphere(in Sphere sphere, in Ray ray, in float t_min, in float t_max, ou
 	return false;
 }
 
+bool world_hit(in Ray ray, in float t_min, in float t_max, out HitRecord hit)
+{
+	HitRecord temp_hit;
+	bool hit_anything = false;
+	float closest_so_far = t_max;
+
+	for(int i=0; i < sphereCount; ++i)
+	{
+		if (sphere_hit(allSpheres[i], ray, t_min, closest_so_far, temp_hit))
+		{
+			hit_anything = true;
+			closest_so_far = temp_hit.t;
+			hit = temp_hit;
+		}
+	}
+
+	return hit_anything;
+}
+
 vec4 color(in Ray ray)
 {
 	const float minRayLength = 0.0;
     const float maxRayLength = 10000.0;
 	HitRecord hit;
-	Sphere sphere = { vec3(0.0, 0.0, -1.0), 0.5 };
 
-	const bool hasHit = hit_sphere(sphere, ray, minRayLength, maxRayLength, hit);
+	const bool hasHit = world_hit(ray, minRayLength, maxRayLength, hit);
 	if (hasHit)
 	{
 		return 0.5 * vec4(hit.normal.x + 1.0, hit.normal.y + 1.0, hit.normal.z + 1.0, 2.0);
