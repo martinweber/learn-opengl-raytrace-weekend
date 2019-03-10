cd5c6e4b285c0ec733423a54c4403bf3853f67d2
Martin Weber <martin.weber@uppercut.at>
2019-03-10 16:08:43 +0100

    added sRGB output to display shader

--- src/shader/display_fs.glsl
+++ src/shader/display_fs.glsl
@@ -3,7 +3,18 @@ uniform sampler2D srcTex;
 in vec2 texCoord;
 out vec4 color;
 
+vec3 LinearToSRGB(in vec3 color)
+{
+    vec3 ret;
+    if (any(greaterThan(color, vec3(0.0031308)))) ret = 1.055 * pow(color, vec3(1.0 / 2.4)) - vec3(0.055);
+    if (color.r <= 0.0031308) { ret.r = color.r < 0.0 ? color.r : color.r * 12.92; }
+    if (color.g <= 0.0031308) { ret.g = color.g < 0.0 ? color.g : color.g * 12.92; }
+    if (color.b <= 0.0031308) { ret.b = color.b < 0.0 ? color.b : color.b * 12.92; }
+    return ret;
+}
+
 void main()
 {
-	color = texture(srcTex, texCoord);
+	vec4 col = texture(srcTex, texCoord);
+	color = vec4(LinearToSRGB(col.rgb), col.a);
 }
